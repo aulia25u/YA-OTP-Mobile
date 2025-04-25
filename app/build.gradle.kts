16e8b3b731b982ea205863595db16dff8842f6b9
CodeSandbox <support@codesandbox.io>
2025-04-25 07:26:39 +0000

    p

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,6 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
-    implementation(libs.androidx.core.splashscreen)
     
     // Retrofit for API calls
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
